c3f1cec80207a32bb1034e530dcafd2a30a170b0
irynamoiseeva <irynamoiseeva@gmail.com>
2017-12-22 01:34:09 +0200

    Initial commit

---
+++ App4/App4/Activity2.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Gms.Maps.Model;
+using Android.Views.InputMethods;
+using System.Resources;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace App4.Resources
+{
+    [Activity(Label = "Activity2")]
+    class Activity2 : Activity
+    {
+        const string strGoogleApiKey = "AIzaSyAqB61v3YI6H7Q-jhx3HVSPNBDvX-dr_yY";
+        const string strGeoCodingUrl = "https://maps.googleapis.com/maps/api/geocode/json";
+        const string strAutoCompleteGoogleApi = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=";
+        //to soft keyboard hide
+        string autoCompleteOptions;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.mytoolbar);
+
+            Button Search = FindViewById<Button>(Resource.Id.Search);
+            Search.Click += Search_Click;
+            var txtSearch = FindViewById<AutoCompleteTextView>(Resource.Id.txtTextSearch);
+            txtSearch.TextChanged += async delegate (object sender, Android.Text.TextChangedEventArgs e)
+            {
+                try
+                {
+                    autoCompleteOptions = await fnDownloadString(strAutoCompleteGoogleApi + txtSearch.Text + "&key=" + strGoogleApiKey);
+
+                    if (autoCompleteOptions == "Exception")
+                    {
+                        Toast.MakeText(this, "Unable to connect to server!!!", ToastLength.Short).Show();
+                        return;
+                    }
+
+                }
+                catch
+                {
+                    Toast.MakeText(this, "Unable to process at this moment!!!", ToastLength.Short).Show();
+                }
+
+            };
+
+        }
+
+
+
+
+
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+
+          /*  var txtSearch = FindViewById<AutoCompleteTextView>(Resource.Id.txtTextSearch);
+            txtSearch.Text = "London";
+            string URL = (strAutoCompleteGoogleApi + txtSearch.Text + "&key=" + strGoogleApiKey);
+
+            InputMethodManager inputManager = (InputMethodManager)this.GetSystemService(Context.InputMethodService);
+
+            inputManager.HideSoftInputFromWindow(txtSearch.WindowToken, HideSoftInputFlags.NotAlways);
+            fnDownloadString(URL);*/
+
+            var txtSearch = FindViewById<AutoCompleteTextView>(Resource.Id.txtTextSearch);
+            Intent myIntent = new Intent(this, typeof(MainActivity));
+            myIntent.PutExtra("city", txtSearch.Text);
+            SetResult(Result.Ok, myIntent);
+            Finish();
+        }
+
+
+        async Task<string> fnDownloadString(string strUri)
+        { var strResultData = "";
+
+            try
+            {
+
+
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(strUri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<GoogleInfo>(content);
+                    var strPredictiveText = new string[data.predictions.Count];
+                    var index = 0;
+                    foreach (Prediction objPred in data.predictions)
+                    {
+                        strPredictiveText[index] = objPred.description;
+                        index++;
+                    }
+                    /*  ArrayAdapter adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleDropDownItem1Line, strPredictiveText);
+                      txtSearch.Adapter = adapter;*/
+
+                    var wordlist = new String[] { "Hello", "Hey", "Heja", "Hi", "Hola", "Bonjour", "Gday" };
+                    ArrayAdapter autoCompleteAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleDropDownItem1Line, strPredictiveText);
+                    var txtSearch = FindViewById<AutoCompleteTextView>(Resource.Id.txtTextSearch);
+                    txtSearch.Adapter = autoCompleteAdapter;
+                }
+            }
+            catch (Exception ex)
+            {
+                //ToDo Give errormessage to user and possibly log error
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+            return strResultData;
+        }
+
+        async void AutoCompleteOption_Click(object sender, AdapterView.ItemClickEventArgs e)
+        {
+           
+
+            /*  InputMethodManager inputManager = (InputMethodManager)this.GetSystemService(Context.InputMethodService);
+
+             inputManager.HideSoftInputFromWindow(txtSearch.WindowToken, HideSoftInputFlags.NotAlways);
+
+             if (txtSearch.Text != string.Empty)
+             {
+                 var sb = new StringBuilder();
+                 sb.Append(strGeoCodingUrl);
+                 sb.Append("?address=").Append(txtSearch.Text);*/
+
+
+            /*    string strResult = await fnDownloadString(sb.ToString());
+                if (strResult == "Exception")
+                {
+                    Toast.MakeText(this, "Unable to connect to server!!!", ToastLength.Short).Show();
+                    //private api_key = "AIzaSyAqB61v3YI6H7Q-jhx3HVSPNBDvX-dr_yY";
+
+                }*/
+        
+    }
+
+    }
+}
---
+++ App4/App4/MainActivity.cs
@@ -0,0 +1,121 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Views;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System;
+
+
+//using Android.Support.V7.App ;
+
+namespace App4
+{
+    [Activity(Label = "App4", MainLauncher = true)]
+    public class MainActivity : Activity
+
+    {
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (resultCode == Result.Ok)
+            {
+                string put_name = data.GetStringExtra("city");
+                var cityname = FindViewById<EditText>(Resource.Id.namecity);
+                cityname.Text = put_name;
+                //put_name and put_position should now hold the results you want, you can do whatever you want with these two values now in your MainActivity
+            }
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Layout.top_menus , menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+           ;
+            switch (item.ItemId)
+            {
+                case Resource.Id.menu_search:
+                    {
+                        Intent i = new Intent(this, typeof(Resources.Activity2));
+                        // StartActivity(i);
+                        StartActivityForResult(i, 0);
+                        break;
+                    }
+                case Resource.Id.menu_preferences:
+                    {
+                        // add your code  
+                        return true;
+                    }
+
+            }
+                    return base.OnOptionsItemSelected(item);
+                    //Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
+                    // ToastLength.Short).Show();
+
+            }
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+         SetActionBar(toolbar);
+           toolbar.Title = "My Toolbar";
+            Button txtCity = FindViewById<Button>(Resource.Id.txtCity );
+            txtCity.Click += txtCity_Click;
+        }
+
+        private void txtCity_Click(object sender, EventArgs e)
+        {
+
+            GetWeather();
+        }
+
+        public async void GetWeather()
+        {
+
+            try
+            {
+               var api_key = "03e8168ffbb8cafb6b8b6679c528ec97";
+                //string URL = ("http://api.openweathermap.org/data/2.5/weather?lat=58.34&lon=11.94&appid="+ api_key);
+
+                var cityname = FindViewById<EditText>(Resource.Id.namecity);
+
+                var citytext = cityname.Text;
+                string URL = ("http://api.openweathermap.org/data/2.5/weather?q="+citytext+"&appid="+ api_key);
+
+
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(new Uri(URL));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var weatherList = JsonConvert.DeserializeObject<RootObject>(content);
+                    
+                    //   var editText = FindViewById<TextView>(Resource.Id.weather_text);
+                    // editText.Text = weatherList.main.ToString();
+                    //weather_text.  weatherList.main;
+                    //Databind the list
+                    //  lstWeather.ItemsSource = weatherList;
+                    var tempr = FindViewById<TextView>(Resource.Id.tempr);
+                    tempr.Text = weatherList.main.temp.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                //ToDo Give errormessage to user and possibly log error
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
+
